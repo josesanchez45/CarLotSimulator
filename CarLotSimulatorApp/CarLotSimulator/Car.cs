58a1d472f268bfa87a0abc3395f50cad171d40bf
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-18 12:05:20 -0700

    initial commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public string MakeEngineNoise()
+        {
+            if (IsDriveable == false)
+            {
+                return "This engine doesn't work";
+            }
+            else
+            {
+                return "Vroom Vroom";
+            }
+
+        }
+        public string MakeHonkNoise()
+        {
+            if (IsDriveable == false)
+            {
+                return "This car doesn't work";
+            }
+            else
+            {
+                return "beep beep";
+            }
+        }
+    }
+
+}       //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+        //The methods should take one string parameter: the respective noise property
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -6,7 +7,7 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
-            //TODO
+            //Done
 
             //Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
@@ -17,9 +18,51 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+           var firstCar = new Car();
+            firstCar.Make = "Ford";
+            firstCar.Model = "Fusion";
+            firstCar.Year = 2014;
+            firstCar.IsDriveable = true;
+            firstCar.EngineNoise = "Vroom";
 
+            var carLot = new List<Car>(); 
+            carLot.Add(firstCar);
+            Console.WriteLine($"Info on {firstCar.Make}");
+            Console.WriteLine($"Engine Noise:  {firstCar.MakeEngineNoise()}");
+            Console.WriteLine($"Horn Noise:  {firstCar.MakeHonkNoise()}");
+            Console.WriteLine();
+
+            var secondCar = new Car() {Make = "Chevy", Model= "Bel-Air", Year = 1956, IsDriveable = false, EngineNoise = "broken" };
+            carLot.Add(secondCar);
+            Console.WriteLine($"Info on {secondCar.Make}");
+            Console.WriteLine($"Engine Noise: {secondCar.MakeEngineNoise()}");
+            Console.WriteLine($"Horn Noise: {secondCar.MakeHonkNoise()}");
+            Console.WriteLine();   
+
+            Car thirdCar = new Car()
+            {
+                Make = "Cadillac",
+                Model= "CTS5",
+                Year = 1962,
+                IsDriveable = true,
+                EngineNoise = "Vroom"
+            };
+            carLot.Add(thirdCar);
+            Console.WriteLine($"Info on {thirdCar.Make}");
+            Console.WriteLine($"Engine Noise: {thirdCar.MakeEngineNoise()}");
+            Console.WriteLine($"Horn Noise: {thirdCar.MakeHonkNoise()}");
+            Console.WriteLine();
             //*************BONUS*************//
-
+            
+            foreach (var car in carLot)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Make: {car.Make}");
+                Console.WriteLine();
+                Console.WriteLine($"Year: {car.Year}");
+                Console.WriteLine();
+                Console.WriteLine($"Model: { car.Model}");
+            }
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
             //*************BONUS X 2*************//
